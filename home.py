04a034540c63ecfa3d21a5bf05fab7c4eebda12b
Rui Coelho <ruicoelho@ua.pt>
2019-12-10 13:19:55 +0000

    Documentation

--- home.py
+++ home.py
@@ -15,6 +15,12 @@ class Home():
                 'definicao': 'Mostrar todas os cartões com acesso a uma determinada casa',
                 'link': '/acessos/<home>',
                 'argumento': 'Número da casa, p.e, 1'
+            },
+            {
+                'titulo': 'Acessos válidos por casa',
+                'definicao' : 'Verifica se um cartão é valido para uma dara casa',
+                'link' : '/isValid/<home>/<card_id>',
+                'argumento' : 'Home, p.e, 1; Card_id, p.e, 01234567'
             }
         ]
         return paths
